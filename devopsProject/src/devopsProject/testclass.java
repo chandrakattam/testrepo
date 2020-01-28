29974f52125cc89d2e587ad621dc6f529a589a70
chandra <chandrasekhar416@gmail.com>
2020-01-28 15:24:30 +0530

    updated class

--- devopsProject/src/devopsProject/testclass.java
+++ devopsProject/src/devopsProject/testclass.java
@@ -3,7 +3,7 @@ package devopsProject;
 public class testclass {
 
 	public static void main(String[] args) {
-		System.out.println("first project");
+		System.out.println("first java project");
 
 	}
 
